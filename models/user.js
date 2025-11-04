d6968112080092286e1a01ec1a110ba2cb1d5c09
Sara Johnson <sgjohnson455@gmail.com>
2025-11-03 23:23:53 -0600

    updated schemas - recheck for submission

--- models/user.js
+++ models/user.js
@@ -1,23 +1,56 @@
 // Load required packages
 var mongoose = require('mongoose');
 
-// Define our user schema
+// Define our user schema (need name/email, other fields default)
 var UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    pendingTasks: [String],
-    dateCreated: Date
+    name: {
+        type: String,
+        required: [true, 'Username needed']
+    },
+    email: {
+        type: String,
+        required: [true, 'User email needed']
+    },
+    pendingTasks: {
+        type: [String],
+        default: [] // default is empty
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-// Define our user schema
+// Define our task schema
 var TaskSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    deadline: Date,
-    completed: Boolean,
-    assignedUser: String,
-    assignedUserName: String,
-    dateCreated: Date
+    name: {
+        type: String,
+        required: [true, 'Task name needed']
+    },
+    description: {
+        type: String,
+        default: ''
+    },
+    deadline: {
+        type: Date,
+        required: [true, 'Task deadline needed']
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    assignedUser: {
+        type: String,
+        default: ''
+    },
+    assignedUserName: {
+        type: String,
+        default: "unassigned"
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 // Export the Mongoose model
